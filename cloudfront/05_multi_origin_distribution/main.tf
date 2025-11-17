27c8bf4befc1871c153ce5f90580539073aff1b6
Daniel Iovescu <iovescu.dani@gmail.com>
2025-11-18 00:33:23 +0200

    added second origin

--- cloudfront/05_multi_origin_distribution/main.tf
+++ cloudfront/05_multi_origin_distribution/main.tf
@@ -1,9 +1,144 @@
-resource "aws_s3_bucket" "static-website" {
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "static_website" {
   bucket        = "cloudfront-05-static-website-bucket"
   force_destroy = true
 }
 
-resource "aws_s3_bucket" "static-assets" {
+resource "aws_s3_bucket" "static_assets" {
   bucket        = "cloudfront-05-static-assets-bucket"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudfront_policy_on_assets" {
+  bucket = aws_s3_bucket.static_assets.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Action = "s3:GetObject"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Resource = "${aws_s3_bucket.static_assets.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.multi_origin_cdn.id}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudfront_policy_on_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Action = "s3:GetObject"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Resource = "${aws_s3_bucket.static_website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.multi_origin_cdn.id}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "s3-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_cache_policy" "assets_cache_policy" {
+  name        = "static-assets-cache-policy"
+  min_ttl     = 0
+  default_ttl = 86400    # 1 day
+  max_ttl     = 31536000 # 1 year
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_distribution" "multi_origin_cdn" {
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "CloudFront multi-origin distribution"
+  default_root_object = "index.html"
+
+  origin {
+    origin_id                = "s3-static-website-origin"
+    domain_name              = aws_s3_bucket.static_website.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  origin {
+    origin_id                = "s3-static-assets-origin"
+    domain_name              = aws_s3_bucket.static_assets.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-static-website-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+
+    cache_policy_id          = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    origin_request_policy_id = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf"
+  }
+
+  ordered_cache_behavior {
+    target_origin_id       = "s3-static-assets-origin"
+    path_pattern           = "/assets/*"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+
+    cache_policy_id = aws_cloudfront_cache_policy.assets_cache_policy.id
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name        = "cloudfront-05-multi-origin-distribution"
+    Description = "CloudFront distribution for multi-origin scenario"
+  }
+
 }
