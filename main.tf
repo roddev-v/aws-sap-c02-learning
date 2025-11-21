8f5cdf7ffa3f9c503917c48b8fd8045ad8cf54d8
Daniel Iovescu <iovescu.dani@gmail.com>
2025-11-22 01:21:04 +0200

    added main module definition

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ module "cloudfornt_02_api_gateway" {
   source = "./cloudfront/02_api_gateway"
 }
 
+module "cloudfront_03_api_gateway_dynamic_cache" {
+  source = "./cloudfront/03_api_gateway_dynamic_cache"
+}
+
 module "cloudfront_05_multi_origin_distribution" {
   source = "./cloudfront/05_multi_origin_distribution"
 }
